486e65bd15e961bc05608b22f89afc015769e96f
Abhiramgc <36700010+Abhiramgc@users.noreply.github.com>
2022-03-19 22:43:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "backend2022"
     key    = "test1/tf.state"
     region = "us-east-1"
   }
-}
+}*/
 
 provider "aws" {
   region = var.region
