bbd61ae2bf89d4eac20408fc8e2d9c45a794795d
Abhiramgc <36700010+Abhiramgc@users.noreply.github.com>
2022-03-19 23:55:23 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "myec2vm" {
   ami = "ami-0c02fb55956c7d316"
-  instance_type = t2.micro
+  instance_type = "t2.micro"
   #user_data = file("${path.module}/app1-install.sh")
   #key_name = var.instance_keypair
   #vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
